15099b91f892af416ec174e0a35ea6f0190ff356
Kamoti <kamotigodfrey2@gmail.com>
2020-07-08 17:52:24 +0300

    Added new files

---
+++ JS/contact.js
@@ -0,0 +1,31 @@
+function validateForm() {
+    var x = document.forms["form"]["firstname"].value;
+    if (x == "") {
+      alert("Fill all the Detials");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var x = document.forms["form"]["lastname"].value;
+    if (x == "") {
+      alert("Fill all the Detials");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var x = document.forms["form"]["emailaddress"].value;
+    if (x == "") {
+      alert("Fill all the Detials");
+      return false;
+    }
+  }
+
+  function validateForm() {
+    var x = document.forms["form"]["comment"].value;
+    if (x == "") {
+      alert("Fill all the Detials");
+      return false;
+    }
+  }
